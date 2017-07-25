d19d8c90c8eeb08f5c7b23aa63b834807cd39a50
Aamir Ahmad <santhu3064@gmail.com>
2017-07-25 21:38:04 +0530

    m

---
+++ .chef/knife.rb
@@ -0,0 +1,10 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "admin"
+client_key               "#{current_dir}/admin.pem"
+validation_client_name   "digitalocean-validator1"
+validation_key           "#{current_dir}/digitalocean-validator.pem"
+chef_server_url          "https://chef-server.local/organizations/digitalocean"
+syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntaxcache"
+cookbook_path            ["#{current_dir}/../cookbooks"]
