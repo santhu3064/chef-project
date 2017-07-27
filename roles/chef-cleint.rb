01e2ef76b7ea49bddefc6c25a47b47f7a69e4cd6
Aamir Ahmad <santhu3064@gmail.com>
2017-07-27 18:31:50 +0530

    databag project changes

--- roles/chef-cleint.rb
+++ roles/chef-cleint.rb
@@ -1,6 +1,6 @@
 name 'newrole'
 description 'practice the role'
-run_list 'recipe[myhaproxy]'
+run_list "recipe[my_users]"
 default_attributes 'apache-test' => {
   'attribute1' => 'from a1',
   'attribute2' => 'great'
